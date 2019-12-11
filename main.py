bd5766c19505d745a7245efafcb6e6aefc25761e
Sour_Tooth <rileyswinson@outlook.com>
2019-12-11 23:49:00 +0000

    The program now has the ability to sort the magicNumber, and had a function that steps the magicNumber once
    Stepping the magicNumber means: highToLow minus lowToHigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-def getMagicNumber():
+def createAMagicNumber():
     numberFound = False
     while numberFound == False:
         randomNumber = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9), random.randint(0, 9) ]
@@ -11,8 +11,19 @@ def getMagicNumber():
             magicNumber = ''
             for number in randomNumber:
                 magicNumber += str(number)
-            
+
+            print(f"Your magicNumber is: {magicNumber}")
             return magicNumber
     
-myMagicNumber = getMagicNumber()
-print(myMagicNumber)
+def takeAStep(numberToStep):
+    lowToHi = ''.join(sorted(myMagicNumber))
+    print(f"Your magicNumber organized from low-high is: {lowToHi}")
+
+    hiToLow = ''.join(sorted(myMagicNumber, reverse = True))
+    print(f"Your magicNumber organized from high-low is: {hiToLow}")
+
+    result = str(int(hiToLow) - int(lowToHi))
+    print(f"Your magicNumber moved one step through the process is: {result}")
+
+myMagicNumber = createAMagicNumber()
+takeAStep(myMagicNumber)
