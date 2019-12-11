e6a6001c1fca05314dc73bc8a966b3ed1ed02ec0
Sour_Tooth <rileyswinson@outlook.com>
2019-12-11 23:24:33 +0000

    Now returns a String instead of a list of numbers

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import random
 
 def getMagicNumber():
     numberFound = False
-
     while numberFound == False:
-        magicNumber = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9), random.randint(0, 9) ]
+        randomNumber = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9), random.randint(0, 9) ]
 
-        if len(magicNumber) == len(set(magicNumber)):
+        if len(randomNumber) == len(set(randomNumber)):
+            numberFound = True
+
+            magicNumber = ''
+            for number in randomNumber:
+                magicNumber += str(number)
+            
             return magicNumber
-        else:
-            numberFound = False
     
 myMagicNumber = getMagicNumber()
 print(myMagicNumber)
---
+++ usermodule.py
@@ -0,0 +1,13 @@
+cycles = 0
+magicNumber = 1000 + cycles
+
+def isAMagicNumber(number):
+
+    if len(number) == len(set(number)):
+        return True
+    else:
+        return False
+ 
+myMagicNumber = getMagicNumber()
+print(myMagicNumber)
+
