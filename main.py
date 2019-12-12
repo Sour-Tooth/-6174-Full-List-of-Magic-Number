7f5abcb6b15f7a84d0daadac5aaac0d93ad9cd3f
Sour-Tooth <rileyswinson@outlook.com>
2019-12-12 00:16:09 +0000

    Combines both

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-import random
+cycles = 0
+magicNumber = 1234
 
-def createAMagicNumber():
-    numberFound = False
-    while numberFound == False:
-        randomNumber = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9), random.randint(0, 9) ]
-
-        if len(randomNumber) == len(set(randomNumber)):
-            numberFound = True
-
-            magicNumber = ''
-            for number in randomNumber:
-                magicNumber += str(number)
-
-            print(f"Your magicNumber is: {magicNumber}")
-            return magicNumber
+def removeDuplicatesFromString(string):
+    result = ''
+    for character in string:
+        if not(character in result):
+            result = result + character
     
+    return result
+
+def isAMagicNumber(number):
+    string = str(number)
+
+    if len(string) == len(removeDuplicatesFromString(string)):
+        return True
+    else:
+        return False
+
 def takeAStep(numberToStep):
     lowToHi = ''.join(sorted(numberToStep))
-    print(f"Your magicNumber organized from low-high is: {lowToHi}")
-
     hiToLow = ''.join(sorted(numberToStep, reverse = True))
-    print(f"Your magicNumber organized from high-low is: {hiToLow}")
-
     result = str(int(hiToLow) - int(lowToHi))
-    print(f"Your magicNumber moved one step through the process is: {result}")
-
     return result
 
 def stepUntilGoalReached(numberToStep):
@@ -32,9 +28,15 @@ def stepUntilGoalReached(numberToStep):
     while numberToStep != '6174':
         numberToStep = takeAStep(numberToStep)
         step += 1
-        print(f"Your number at step {step} is: {numberToStep}")
+    
+    print(f"Your magicNumber {magicNumber} took {step} steps to reach 6174.")
 
     return numberToStep
-        
-myMagicNumber = createAMagicNumber()
-myMagicNumber = stepUntilGoalReached(myMagicNumber)
+
+while magicNumber <= 9999:
+    magicNumber = 1000 + cycles 
+
+    if isAMagicNumber(magicNumber):
+        stepUntilGoalReached(str(magicNumber))
+
+    cycles += 1 
