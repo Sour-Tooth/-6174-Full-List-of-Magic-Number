449f993ce9d71a1bdcbf73baa7c16cc34b263d98
Sour-Tooth <rileyswinson@outlook.com>
2019-12-12 00:17:08 +0000

    Every magicNumber is now stepped up to 6174. the number + the amount of steps used is now displayed

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 cycles = 0
 magicNumber = 1234
 
+def removeDuplicatesFromString(string):
+    result = ''
+    for character in string:
+        if not(character in result):
+            result = result + character
+    
+    return result
+
+def isAMagicNumber(number):
+    string = str(number)
+
+    if len(string) == len(removeDuplicatesFromString(string)):
+        return True
+    else:
+        return False
+
+def takeAStep(numberToStep):
+    lowToHi = ''.join(sorted(numberToStep))
+    hiToLow = ''.join(sorted(numberToStep, reverse = True))
+    result = str(int(hiToLow) - int(lowToHi))
+    return result
+
+def stepUntilGoalReached(numberToStep):
+    step = 0
+    while numberToStep != '6174':
+        numberToStep = takeAStep(numberToStep)
+        step += 1
+    
+    print(f"Your magicNumber {magicNumber} took {step} steps to reach 6174.")
+
+    return numberToStep
+
 while magicNumber <= 9999:
-    magicNumber = 1000 + cycles
-
-    def removeDuplicatesFromString(string):
-        result = ''
-        for character in string:
-            if not(character in result):
-                result = result + character
-        
-        return result
-
-    def isAMagicNumber(number):
-        string = str(number)
-
-        if len(string) == len(removeDuplicatesFromString(string)):
-            return True
-        else:
-            return False
+    magicNumber = 1000 + cycles 
 
     if isAMagicNumber(magicNumber):
-        print(magicNumber)
+        stepUntilGoalReached(str(magicNumber))
 
     cycles += 1 
